26463ad2a481e978da676384bc8e51243ec4136b
Brian Johnson <brian.johnson5145@gmail.com>
2018-04-19 21:17:29 -0500

    Lab2

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //This is a comment.
+            //This is a comment.
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WhileIteration/WhileIteration/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace WhileIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+               displayMenu =  MainMenu();
+            }
+            
+        }
+
+        private static bool MainMenu()
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Option 1");
+            Console.WriteLine("2) Option 2");
+            Console.WriteLine("3) Exit");
+            string result = Console.ReadLine();
+            if (result == "1")
+            {
+                PrintNumbers();
+                return true;
+            }
+            else if (result == "2")
+            {
+                GuessingGame();
+                return true;
+            }
+            else if (result == "3")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private static void PrintNumbers()
+        {
+            Console.WriteLine("Print Numbers !");
+            Console.WriteLine("Type a number: ");
+            int result = int.Parse(Console.ReadLine());
+            int counter = 1;
+            while (counter<result)
+            {
+                Console.Write("-");
+                counter++;
+            }
+            Console.WriteLine();
+        }
+        private static void GuessingGame()
+        {
+            Console.WriteLine("Guessing Game!");
+            Console.WriteLine();
+        }
+    }
+}
