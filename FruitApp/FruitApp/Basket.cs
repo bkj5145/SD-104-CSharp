03ea63fa86a9da48306aac5a6ec0f5ee653086df
Brian Johnson <brian.johnson5145@gmail.com>
2018-05-16 21:44:59 -0500

    first commit

---
+++ FruitApp/FruitApp/Basket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitApp
+{
+     public class Basket : Fruit
+    {
+        private List<Fruit> ListOfFruits  ;
+
+        public Basket()
+        {
+            ListOfFruits = new List<Fruit>(); 
+        }
+
+       
+
+        public void Throw(Fruit fruit)
+        {
+            ListOfFruits.Remove(fruit);
+        }
+
+        public void DisplayFruits()
+        {
+
+            for (int i = 0; i < ListOfFruits.Count; i++)
+            {
+                Console.WriteLine(ListOfFruits[i].FruitName);
+            }
+           
+        }
+
+        public void AddFruit(Fruit fruits)
+        {
+            ListOfFruits.Add(fruits);
+        }
+    }
+}
---
+++ FruitApp/FruitApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Basket myBasket = new Basket();
+
+            Banana mybanana = new Banana();
+            Mango myMango = new Mango();
+            Apple myApple = new Apple();
+
+
+            myBasket.AddFruit(mybanana);
+            myBasket.AddFruit(myMango);
+            myBasket.AddFruit(myApple);
+
+            myBasket.Throw(myApple);
+
+            myBasket.DisplayFruits();
+
+            Console.ReadLine();
+            
+        }
+    }
+}
